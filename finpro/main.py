f05ec6f1af955981413ded3af149e8baa6bf7a84
wilsonjih_msi <nssh255297@gmail.com>
2020-12-23 01:33:50 +0800

    main.py

--- finpro/main.py
+++ finpro/main.py
@@ -1,7 +1,9 @@
 import csv
 
+data = []
 
-with open('CBPL_ppit_2019_data.csv', newline='', encoding='utf-8') as csvfile:
+with open('CBPL_ppit_2019_data.csv', newline='', encoding='utf-8-sig') as csvfile:
     rows = csv.reader(csvfile)
     for row in rows:
-        print(row)
+    	data.append(row)
+print(data)
